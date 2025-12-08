6c98be6881d09c87144be334ab5e893f224392ff
Guillermo <md.guillermo1@gmail.com>
2025-12-08 21:15:15 +0100

    module00

---
+++ module00/ex5/ft_count_harvest_iteractive.py
@@ -0,0 +1,7 @@
+def ft_count_harvest_iteractive():
+    days = int(input("Days untl harvest: "))
+    days = range(days)
+    for n in days:
+        print("Day ",n)
+    print("Harvest time!")
+    
