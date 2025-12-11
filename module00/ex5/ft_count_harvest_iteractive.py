78cc6a82515ace7219dac7ee905996c8bb3ed4e0
Guillermo Munoz <gumunoz@c2r18s4.42madrid.com>
2025-12-11 15:43:41 +0100

    project finished

--- module00/ex5/ft_count_harvest_iteractive.py
+++ module00/ex5/ft_count_harvest_iteractive.py
@@ -2,6 +2,5 @@ def ft_count_harvest_iteractive():
     days = int(input("Days untl harvest: "))
     days = range(days)
     for n in days:
-        print("Day ",n)
+        print("Day ", n)
     print("Harvest time!")
-    
--- module00/ex5/ft_count_harvest_recursive.py
+++ module00/ex5/ft_count_harvest_recursive.py
@@ -1,1 +1,12 @@
-def ft_
+def ft_count_harvest_recursive():
+    days = int(input("Enter number of days: "))
+    init = 1
+
+    def recursive(days: int, init: int) -> None:
+        print(f"Day: {init}")
+        if days == 1:
+            return
+        init += 1
+        recursive(days - 1, init)
+    recursive(days, init)
+    print("Harvest time!")
