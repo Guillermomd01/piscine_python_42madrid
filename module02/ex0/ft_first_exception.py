84fdc7df1edea71e438a34bf8297486492e26b5a
Guillermo Munoz <gumunoz@c1r11s6.42madrid.com>
2025-12-17 15:14:02 +0100

    error in except

--- module02/ex0/ft_first_exception.py
+++ module02/ex0/ft_first_exception.py
@@ -1,4 +1,4 @@
-def check_temperature(temp_str:str):
+def check_temperature(temp_str: str) -> None:
     print(f"Testing temperature: {temp_str}")
     temperature = None
     try:
@@ -6,18 +6,18 @@ def check_temperature(temp_str:str):
     except:
         print(f"Error '{temp_str}' is not a valid number")
     if temperature is not None:
-        
+
         if temperature >= 0 and temperature <= 40:
             print(f"Temperature {temperature}ºC i sperfect for plants!")
         elif temperature > 40:
             print(f"Error: {temperature}ºC is too hot for plants (max 40ºC)")
         elif temperature < 0:
             print(f"Error: {temperature}ºC is too cold for plants (min 0ºC)")
-    #print("All test completed - program didnt´t crash!")
-    
-if __name__ == "__main__":
-    check_temperature("25")
-    check_temperature("abc")
-    check_temperature("100")
-    check_temperature(-50)
-    check_temperature("-50")
+    print("All test completed - program didnt't crash!")
+
+
+check_temperature("25")
+check_temperature("abc")
+check_temperature("100")
+check_temperature(-50)
+check_temperature("-50")
