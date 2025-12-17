2fa8a81d779aebec77a1390f34a1b817253de8ae
Guillermo <md.guillermo1@gmail.com>
2025-12-17 17:40:40 +0100

    tuple

--- module03/ex2/ft_coordinate_system.py
+++ module03/ex2/ft_coordinate_system.py
@@ -1,4 +1,7 @@
 import sys
 import math
 
-vf
+tupla1 = tuple(sys.argv[1:])
+print(f"position created: {tupla1}")
+distance = math.sqrt((int(tupla1[0]) - 0) ** 2 + (int(tupla1[1]) - 0) ** 2 + (int(tupla1[2]) - 0) ** 2)
+print(f"Distance between (0, 0, 0) and {tupla1}: {distance:.2f}")
