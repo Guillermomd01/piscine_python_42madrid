9022ffb740dd4cd6e7dd9966bc6c33b047bdd48d
Guillermo Munoz <gumunoz@c1r11s6.42madrid.com>
2025-12-17 15:15:53 +0100

    error except ex1

--- module03/ex1/ft_score_analytics.py
+++ module03/ex1/ft_score_analytics.py
@@ -1,4 +1,24 @@
 import sys
 print("=== Player Score Analytics ===")
-for arg in sys.argv:
-    
+if len(sys.argv) < 2:
+    print(
+        f"No scores provided. Usage python3 {sys.argv[0]} "
+        f"<score1> <score2> ...")
+else:
+    numbers_list = []
+    error = False
+    try:
+        for arg in sys.argv[1:]:
+            numbers_list.append(int(arg))
+    except:
+        print(f"{arg} is not a number")
+        error = True
+    if not error:
+        total = sum(numbers_list)
+        average = total / len(numbers_list)
+        print(f"Scores processed: {numbers_list}")
+        print(f"Total players: {len(numbers_list)}")
+        print(f"Total score: {total}")
+        print(f"Average score: {average:.1f}")
+        print(f"High score: {max(numbers_list)}")
+        print(f"Low score: {min(numbers_list)}")
