1e1bbd4001976f36cbddd2baa5f1e5e72f4ca1f4
Guillermo <md.guillermo1@gmail.com>
2025-12-19 21:58:46 +0100

    creation tuple and parsed, only try/except case left

--- module03/ex2/ft_coordinate_system.py
+++ module03/ex2/ft_coordinate_system.py
@@ -1,7 +1,24 @@
 import sys
 import math
 
-tupla1 = tuple(sys.argv[1:])
-print(f"position created: {tupla1}")
-distance = math.sqrt((int(tupla1[0]) - 0) ** 2 + (int(tupla1[1]) - 0) ** 2 + (int(tupla1[2]) - 0) ** 2)
-print(f"Distance between (0, 0, 0) and {tupla1}: {distance:.2f}")
+origin_coords = (0, 0, 0)
+created_coords = (10, 20, 5)
+print(f"Position created: {created_coords}")
+x0, y0, z0 = origin_coords
+x1, y1, z1 = created_coords
+distance = math.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2 + (z1 - z0) ** 2)
+print(f"Distance between {origin_coords} and {created_coords}: {distance:.2f}")
+
+string_coords = "3,4,0"
+print(f"Parsing coordinates: {string_coords}")
+split_coords = string_coords.split(",")
+parsed_coords = []
+for coords in split_coords:
+    parsed_coords.append(int(coords))
+parsed_coords = tuple(parsed_coords)
+print(f"Parsed position: {parsed_coords}")
+x2, y2, z2 = parsed_coords
+distance = math.sqrt((x2 - x0) ** 2 + (y2 - y0) ** 2 + (z2 - z0) ** 2)
+print(f"Distance between {origin_coords} and {parsed_coords}: {distance:.1f}")
+
+print(sys.argv[1:])
