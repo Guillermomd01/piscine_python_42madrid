43392a58bafabcd75c76501bf96bc2dd89f831bf
Guillermo <md.guillermo1@gmail.com>
2025-12-23 10:28:57 +0100

    creating a inventory

---
+++ module03/ex4/ft_inventory_system.py
@@ -0,0 +1,11 @@
+print("=== Player Inventory System ===\n")
+
+print("=== Alice's Inventory ===")
+
+
+
+print("=== Transaction : ")
+
+print("=== Upadted Inventory ===")
+
+print("=== Inventory Analytics ===")
