aff6541767eb0b772ab06eeb143b7cfd4fed607f
Guillermo <md.guillermo1@gmail.com>
2026-01-19 18:52:33 +0100

    flake8

--- module09/module09/ex2/space_crew.py
+++ module09/module09/ex2/space_crew.py
@@ -3,6 +3,7 @@ from enum import Enum
 from datetime import datetime
 from typing import List
 
+
 class Rank(str, Enum):
     cadet = "cadet"
     officer = "officer"
@@ -30,23 +31,27 @@ class SpaceMission(BaseModel):
     crew: List[CrewMember] = Field(ge=1, le=12)
     mission_status: str = Field(default="planned")
     budget_millions: float = Field(ge=1, le=10000)
+
     @model_validator(mode='after')
     def mission_validation_rules(self):
         if self.mission_id[:1] != "M":
             raise ValueError('Mission ID must start with "M"')
-        leader = any(m.rank in [Rank.captain, Rank.commander] for m in self.crew)
+        leader = any(
+            m.rank in [Rank.captain, Rank.commander] for m in self.crew)
         if not leader:
-            raise ValueError("Mission must have at least one Commander or Captain")
+            raise ValueError(
+                "Mission must have at least one Commander or Captain")
         len_crew = len(self.crew)
-        most_experience = [member.years_experience for member in self.crew if member.years_experience >= 5]
+        most_experience = [
+            member.years_experience for member in self.crew
+            if member.years_experience >= 5]
         percent_experience_member = len(most_experience) / len_crew
         if self.duration_days > 365 and percent_experience_member < 0.5:
-            raise ValueError("Long missions (> 365 days) need 50% experienced crew (5+ years)")
+            raise ValueError(
+                "Long missions (>365 days)"
+                "need 50% experienced crew (5+ years)"
+                )
         for members in self.crew:
-            if members.is_active == False:
+            if members.is_active is False:
                 raise ValueError("All crew members must be active")
         return self
-
-
-def main():
-    
